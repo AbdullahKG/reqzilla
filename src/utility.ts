71bba477e40bb9d51a205c4c1590ba664be6c15f
Abdullah Omar <abdullah.aboode.omar@gmail.com>
2025-04-02 18:58:46 +0300

    add new function to save the response

--- src/utility.ts
+++ src/utility.ts
@@ -1,3 +1,7 @@
+import fs from 'fs';
+import { Readable } from 'stream';
+import chalk from 'chalk';
+
 export function parseHeaders(headers: string[]) {
   return headers
     ? Object.fromEntries(
@@ -7,4 +11,37 @@ export function parseHeaders(headers: string[]) {
         }),
       )
     : {};
+}
+
+export async function saveResponseToFile(response: any, filePath: string) {
+  try {
+    const metadata = {
+      statusCode: response.status,
+      headers: response.headers,
+    };
+
+    if (response.body instanceof Readable) {
+      // If body is a stream, write metadata first, then append the stream
+      fs.writeFileSync(filePath, JSON.stringify(metadata, null, 2) + '\n\n');
+
+      const fileStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+      response.body.pipe(fileStream);
+      fileStream.on('finish', () => {
+        console.log(chalk.green(`File written successfully to: ${filePath}`));
+      });
+    } else {
+      // If body is JSON or string, store it with metadata
+      const dataToSave = {
+        ...metadata,
+        body: response.data,
+      };
+
+      const data: string = JSON.stringify(dataToSave, null, 2);
+      fs.writeFileSync(filePath, data);
+      console.log(chalk.green(`File written successfully to: ${filePath}\n`));
+    }
+  } catch (err: any) {
+    console.error(chalk.red(`Error while writing to file: ${err.message}`));
+  }
 }
