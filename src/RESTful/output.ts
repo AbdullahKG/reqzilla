ad2cc4c41716e1aeae66793e576879d74d9a5a5e
Abdullah Omar <abdullah.aboode.omar@gmail.com>
2025-04-03 19:00:33 +0300

    implemented two function verboseOutput and displayOnlyHeaders

--- src/RESTful/output.ts
+++ src/RESTful/output.ts
@@ -2,6 +2,15 @@ import chalk from 'chalk';
 
 export function formatResponse(response: any) {
   console.log(chalk.blue('\nStatus:'), response.status);
+  console.log(chalk.blue('\nData:'), JSON.stringify(response.data, null, 2));
+}
+
+export function verboseOutput(response: any) {
+  console.log(chalk.blue('\nStatus:'), response.status);
   console.log(chalk.blue('\nHeaders:'), response.headers);
   console.log(chalk.blue('\nData:'), JSON.stringify(response.data, null, 2));
+}
+
+export function displayOnlyHeaders(response: any) {
+  console.log(chalk.blue('\nHeaders:'), response.headers);
 }
